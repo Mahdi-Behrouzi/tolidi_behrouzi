ffbc7c82600860dca73c83d5a38cd484ae920364
tolidibehrouzi <97166333+Mahdi-Behrouzi@users.noreply.github.com>
2025-04-30 12:54:32 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.getElementById('menu-toggle');
+const sidebar = document.getElementById('sidebar');
+
+menuToggle.addEventListener('click', function() {
+  sidebar.classList.toggle('active');
+});
