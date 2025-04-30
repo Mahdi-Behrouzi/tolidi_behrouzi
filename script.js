3f030d6f93de3ada25da38c21bb7430bc27c2c35
tolidibehrouzi <97166333+Mahdi-Behrouzi@users.noreply.github.com>
2025-04-30 15:00:49 +0330

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,3 @@ const sidebar = document.getElementById('sidebar');
 menuToggle.addEventListener('click', function() {
   sidebar.classList.toggle('active');
 });
-// منو کشویی
-const menuToggle = document.getElementById('menu-toggle');
-const sidebar = document.getElementById('sidebar');
-
-menuToggle.addEventListener('click', function() {
-  sidebar.classList.toggle('active');
-});
-
-// وقتی صفحه کامل لود شد، لودینگ رو حذف کن
-window.addEventListener('load', () => {
-  const loader = document.getElementById('loader');
-  loader.style.display = 'none';
-});
