ee66506d4e7a31b5b850eae0517dcf475eaa89b5
tolidibehrouzi <97166333+Mahdi-Behrouzi@users.noreply.github.com>
2025-05-01 14:22:01 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-const menuToggle = document.getElementById('menuToggle');
-const sidebar = document.getElementById('sidebar');
 
-menuToggle.addEventListener('click', function() {
-  sidebar.classList.toggle('active');
-});
