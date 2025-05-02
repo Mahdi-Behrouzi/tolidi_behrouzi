f916ca2593d53fa9b35fbf4953d5b891bdd480c5
tolidibehrouzi <97166333+Mahdi-Behrouzi@users.noreply.github.com>
2025-05-02 15:34:45 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// باز و بسته کردن منو
+const toggleButton = document.querySelector('.menu-toggle');
+const sidebar = document.querySelector('.sidebar');
 
+toggleButton.addEventListener('click', () => {
+  sidebar.classList.toggle('active');
+});
+
+// شمارنده بازدید
+const visitCountElem = document.getElementById('visit-count');
+let count = localStorage.getItem('visitCount');
+
+if (count) {
+  count = Number(count) + 1;
+} else {
+  count = 1;
+}
+localStorage.setItem('visitCount', count);
+visitCountElem.textContent = `تعداد بازدید: ${count}`;
