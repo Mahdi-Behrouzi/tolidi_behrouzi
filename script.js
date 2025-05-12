d6b4e3fc6977ca3d6cdae71b43a4db25741f8df3
tolidibehrouzi <97166333+Mahdi-Behrouzi@users.noreply.github.com>
2025-05-12 14:32:25 +0330

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ const nav = document.getElementById('nav');
 
 menuToggle.addEventListener('click', () => {
   nav.classList.toggle('show');
+  // عوض کردن دکمه سه خطی به ضربدر و بالعکس
+  if (nav.classList.contains('show')) {
+    menuToggle.innerHTML = '&#10006;'; // ✖ ضربدر
+  } else {
+    menuToggle.innerHTML = '&#9776;';  // ☰ سه خط
+  }
 });
