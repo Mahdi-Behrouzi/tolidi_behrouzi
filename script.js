9586afebd056aa8cac6cafcc001fe98e0c87373a
tolidibehrouzi <97166333+Mahdi-Behrouzi@users.noreply.github.com>
2025-05-13 11:54:12 +0330

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ menuToggle.addEventListener('click', () => {
     menuToggle.innerHTML = '&#9776;';  // ☰ سه خط
   }
 });
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slide");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 2000); // تغییر هر ۲ ثانیه
+}
