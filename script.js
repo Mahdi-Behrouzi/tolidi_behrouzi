dfb81a54abb0f2c400fa350836bd387209ede099
tolidibehrouzi <97166333+Mahdi-Behrouzi@users.noreply.github.com>
2025-05-14 23:16:15 +0330

    Update script.js

--- script.js
+++ script.js
@@ -22,31 +22,3 @@ function showSlide() {
 }
 
 setInterval(showSlide, 2000);
-<script>
-  const slides = document.querySelectorAll(".slide");
-  const prev = document.querySelector(".prev");
-  const next = document.querySelector(".next");
-
-  let index = 0;
-
-  function showSlide(i) {
-    slides.forEach(slide => slide.classList.remove("active"));
-    slides[i].classList.add("active");
-  }
-
-  function nextSlide() {
-    index = (index + 1) % slides.length;
-    showSlide(index);
-  }
-
-  function prevSlide() {
-    index = (index - 1 + slides.length) % slides.length;
-    showSlide(index);
-  }
-
-  next.addEventListener("click", nextSlide);
-  prev.addEventListener("click", prevSlide);
-
-  // تغییر خودکار هر 3 ثانیه
-  setInterval(nextSlide, 3000);
-</script>
