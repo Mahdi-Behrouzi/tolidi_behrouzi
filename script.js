bb3afb22ef6931fbd93f40f9877c70bc797fbfe8
tolidibehrouzi <97166333+Mahdi-Behrouzi@users.noreply.github.com>
2025-07-22 21:55:43 +0330

    Update script.j

--- script.js
+++ script.js
@@ -19,10 +19,3 @@ function closeMenu() {
   menuBtn.setAttribute('aria-label', 'بازکردن منو');
 }
 <script>
-  const rangeInput = document.getElementById('rangeInput');
-  const rangeValue = document.getElementById('rangeValue');
-
-  rangeInput.addEventListener('input', () => {
-    rangeValue.textContent = rangeInput.value;
-  });
-</script>
