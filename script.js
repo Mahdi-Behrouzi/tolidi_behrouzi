086abe2793d2ca18ee4a27548df7f95f06f7e7d9
tolidibehrouzi <97166333+Mahdi-Behrouzi@users.noreply.github.com>
2025-07-22 21:31:38 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const menuBtn = document.getElementById('menu-btn');
+const sideMenu = document.getElementById('side-menu');
+const closeBtn = document.getElementById('close-btn');
+
+menuBtn.addEventListener('click', () => {
+  sideMenu.classList.add('open');
+});
+
+closeBtn.addEventListener('click', () => {
+  sideMenu.classList.remove('open');
+});
+
+// بستن منو وقتی روی لینک‌ها کلیک می‌شه (برای تجربه بهتر موبایل)
+function closeMenu() {
+  sideMenu.classList.remove('open');
+}
+
+// اسلایدر
 let slides = document.querySelectorAll('.slide');
 let index = 0;
 
@@ -8,7 +26,3 @@ function showNextSlide() {
 }
 
 setInterval(showNextSlide, 2000);
-
-
-
-
